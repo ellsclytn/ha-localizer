25010a998d5d9a1cda25d94f448ba248c138027f
Ellis Clayton <ellis@ellis.codes>
2026-02-15 15:26:03 +1100

    feat: improve error handling
    Avoids the use of unwrap, propoagates errors up to a single place for
    running `exit(1)`. Also fixes the timezone existence check:
    `std::fs::exists` only returns an `Err` if it was unable to verify
    existence (e.g. permissions issue). A missing timezone will return
    `Ok(false)`.

--- src/bin/ha-timezone-sync/main.rs
+++ src/bin/ha-timezone-sync/main.rs
@@ -1,13 +1,21 @@
+use anyhow::{Result, bail};
 use ha_localizer::{config::Config, home_assistant::Client};
 use serde::{Deserialize, Serialize};
 use std::{fs, os::unix::fs::symlink, process};
 
 const ETC_LOCALTIME: &str = "/etc/localtime";
+const ZONEINFO_PATH: &str = "/usr/share/zoneinfo";
 
 fn main() {
     let provider = TimezoneProvider::new();
 
-    provider.sync_timezone();
+    match provider.sync_timezone() {
+        Ok(tz) => println!("Timezone set to {tz}"),
+        Err(e) => {
+            eprintln!("{e}");
+            process::exit(1);
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -38,34 +46,36 @@ impl TimezoneProvider {
         TimezoneProvider { client }
     }
 
-    pub fn sync_timezone(&self) {
-        let tz = self.get_timezone().attributes.time_zone_id;
+    pub fn sync_timezone(&self) -> Result<String> {
+        let tz = self.get_timezone()?.attributes.time_zone_id;
+        self.set_timezone(&tz)?;
 
-        self.set_timezone(&tz);
+        Ok(tz)
     }
 
-    fn get_timezone(&self) -> TimezoneResponse {
+    fn get_timezone(&self) -> Result<TimezoneResponse> {
         let path = format!(
             "/api/states/sensor.{}_current_time_zone",
             self.client.device_id
         );
+        let response: TimezoneResponse = self.client.get(&path).send()?.json()?;
 
-        let response: TimezoneResponse = self.client.get(&path).send().unwrap().json().unwrap();
-
-        response
+        Ok(response)
     }
 
-    fn set_timezone(&self, tz: &str) {
-        let original = format!("/usr/share/zoneinfo/{tz}");
-
-        if let Err(e) = fs::exists(&original) {
-            eprintln!("Failed to find {tz}: {e}");
-            process::exit(1);
+    fn set_timezone(&self, tz: &str) -> Result<()> {
+        let original = format!("{ZONEINFO_PATH}/{tz}");
+        match fs::exists(&original) {
+            Ok(false) => bail!("{tz} does not exist in zoneinfo."),
+            Err(e) => bail!(
+                "Unable to verify whether {tz} exists. Check the application has permission to read {ZONEINFO_PATH}. {e}"
+            ),
+            _ => {}
         }
 
-        fs::remove_file(ETC_LOCALTIME).unwrap();
-        symlink(original, ETC_LOCALTIME).unwrap();
+        fs::remove_file(ETC_LOCALTIME)?;
+        symlink(original, ETC_LOCALTIME)?;
 
-        println!("Timezone set to {tz}");
+        Ok(())
     }
 }
